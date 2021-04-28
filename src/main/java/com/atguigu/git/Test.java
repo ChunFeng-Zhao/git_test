59acab957f4bd5d357a39e7cbc8317326561417a
Zcf <1606824405@qq.com>
2021-04-28 15:27:33 +0800

    push commit

--- src/main/java/com/atguigu/git/Test.java
+++ src/main/java/com/atguigu/git/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println("Hello Java");
         System.out.println("master is update subject");
         System.out.println("hot-fix is update subject");
+        System.out.println("push test for github");
     }
 }
